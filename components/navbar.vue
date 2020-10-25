f95867e158897bde85c175f0c2cf340c22422afb
Ayush Shah <ayush02shah12@gmail.com>
2020-10-25 22:30:17 +0530

    Base

---
+++ components/navbar.vue
@@ -0,0 +1,40 @@
+<template>
+  <nav>
+    <ul>
+      <li>Supra</li>
+      <li>Home</li>
+      <li>Products</li>
+      <li>About Us</li>
+      <li>Contact Us</li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style>
+ul {
+  list-style: none;
+  display: flex;
+}
+
+li:nth-child(1) {
+  margin-right: auto;
+  font-size: 1.2em;
+  font-weight: 600;
+}
+li:hover {
+  text-shadow: 1px 1px 0px rgba(0, 0, 0, 0.33333);
+}
+li {
+  margin: 0 2.5%;
+  font-size: 0.8em;
+  text-transform: uppercase;
+  font-weight: 300;
+  transition: 0.5s all ease-in-out;
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Navbar />
+    <Nuxt />
+  </div>
+</template>
+<script>
+import Navbar from "@/components/navbar";
+export default {
+  components: { Navbar },
+};
+</script>
+<style>
+* {
+  box-sizing: border-box;
+}
+html,
+body {
+  margin: 0;
+  padding: 0;
+  font-family: Open Sans, sans-serif;
+}
+</style>
